137a468c185945e3689a9d607f8ea614fba55f34
Laura <lauracontreras19102003@gmail.com>
2023-09-11 15:58:47 -0600

    05 Metodos y propiedades stringss

---
+++ strings2.js
@@ -0,0 +1,10 @@
+// Metodos y propiedades Strings 
+const text = 'Este es un texto cool';
+console.log (text.length);
+console.log(text.toUpperCase());
+console.log(text.toLocaleUpperCase());
+console.log(text.charAt(2));
+console.log(text.indexOf('T'));
+console.log(text.startsWith('T'));
+console.log(text.endsWith('E'));
+console.log('*'.repeat(10));
